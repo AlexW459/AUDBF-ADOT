947497802be50ba0967e812449c5df00c5d2f132
AWS ParallelCluster user <ubuntu@ip-10-255-0-163.ap-southeast-2.compute.internal>
2026-06-21 08:10:54 +0000

    Ran meshing on cluster to check results

---
+++ Final_ADOT_program/Aerodynamics_Simulation_0/runSim.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# First argument is endTime
+# Second argument is timeStep, 
+# Next argument is the case number
+# Next argument is the number of nodes
+# Next argument is the number of processes per node
+# Final argument is the location of the openfoam source script
+
+#Loads latest Openfoam version
+openfoamSource=$6
+
+. $openfoamSource
+
+#Enters case
+caseNum="Aerodynamics_Simulation_$3"
+cd $caseNum
+
+#Sets controls for simulation
+sed -i "/endTime/c\endTime         $1;" system/controlDict
+sed -i "/deltaT/c\deltaT          $2;" system/controlDict
+
+#Replaces files in zero directory
+rm -r -f 0/*
+cp initialValues/* 0/
+cp -r constant/polyMesh 0/
+
+#Gets total number of processes
+totalProcesses=$(($4*$5))
+
+#Updates number of processes
+sed -i "/numberOfSubdomains/c\numberOfSubdomains       $totalProcesses;" system/decomposeParDict
+
+decomposePar -force > decomposeLog 
+
+# Load the Intel oneAPI environment for the job
+#source /opt/intel/oneapi/setvars.sh
+
+# Set the PMI library path for Slurm-MPI integration
+export I_MPI_PMI_LIBRARY=/opt/slurm/lib/libpmi.so
+
+#Clears postprocessing files
+rm -r -f postProcessing/aeroForces/0/*
+rm -r -f postProcessing/tailAeroForces/0/*
+rm -r -f postProcessing/tailUpstreamVelocity/0/*
+
+
+#Sets up slurm script
+sed -i "$((2))s/.*/#SBATCH --job-name=ADOT-Meshing_$3/" simParallel.sh
+sed -i "$((3))s/.*/#SBATCH --nodes=$4/" simParallel.sh
+sed -i "$((4))s/.*/#SBATCH --ntasks-per-node=$5/" simParallel.sh
+sed -i "/bashrc/c\ . $openfoamSource \\\ " meshParallel.sh
+
+
+
+
+echo "Running simulation in parallel on rank $3 on $totalProcesses processes across $4 nodes"
+
+potentialFoam -writep > potentialLog
+foamRun -solver incompressibleFluid > simLog
+#sbatch --wait --wait-all-nodes 1 simParallel.sh
+
+rm -r -f processor*
---
+++ Final_ADOT_program/Aerodynamics_Simulation_1/meshObj.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#First argument is the case number
+#Second number is the number of nodes to run on
+#Third argument is the processes per node
+#Fourth argument is the location of the OpenFOAM sourcing script
+
+openfoamSource=$4
+
+
+#Enters case
+caseNum="Aerodynamics_Simulation_$1"
+cd $caseNum
+
+#Gets total number of processes
+totalProcesses=$(($2*$3))
+
+#Loads latest Openfoam version
+#. /usr/lib/openfoam/openfoam2512/etc/bashrc
+. $openfoamSource
+
+#Cleans mesh
+surfaceSplitByTopology aircraftMesh/aircraftModelRaw.obj splitPatches/aircraftModelSplit.obj > splitLog
+
+#Only retains largest mesh
+largestAircraftFile=$(wc -l *splitPatches/aircraftModelSplit_*.obj | sort -n | tail -n 2 | head -n 1 | awk '{print $2}')
+cp $largestAircraftFile aircraftMesh/aircraftModelSplit.obj
+rm splitPatches/aircraftModel*
+
+surfaceLambdaMuSmooth aircraftMesh/aircraftModelSplit.obj aircraftMesh/aircraftModel.obj 0.5 0.5 20 > smoothLog
+gzip aircraftMesh/aircraftModel.obj
+
+
+rm -f constant/geometry/aircraftModel*
+cp aircraftMesh/aircraftModel.obj.gz constant/geometry/
+
+
+rm -r -f constant/polyMesh/*
+rm -r -f 0/*
+
+rm -r -f constant/extendedFeatureEdgeMesh/*
+
+#Updates number of processes
+sed -i "/numberOfSubdomains/c\numberOfSubdomains       $totalProcesses;" system/decomposeParDict
+
+
+blockMesh > blockLog
+surfaceFeatures > surfaceLog
+
+decomposePar -force -constant > decomposeLog
+
+# Load the Intel oneAPI environment for the job
+#source /opt/intel/oneapi/setvars.sh
+
+# Set the PMI library path for Slurm-MPI integration
+export I_MPI_PMI_LIBRARY=/opt/slurm/lib/libpmi.so
+
+echo "Running snappyHexMesh in parallel on rank $1 on $totalProcesses processes across $2 nodes"
+
+#Sets up slurm script
+sed -i "$((2))s/.*/#SBATCH --job-name=ADOT-Meshing_$1/" meshParallel.sh
+sed -i "$((3))s/.*/#SBATCH --nodes=$2/" meshParallel.sh
+sed -i "$((4))s/.*/#SBATCH --ntasks-per-node=$3/" meshParallel.sh
+sed -i "/bashrc/c\ . $openfoamSource \\\ " meshParallel.sh
+
+#snappyHexMesh -overwrite > meshLog
+sbatch --wait --wait-all-nodes 1 meshParallel.sh
+
+reconstructPar -constant > reconstructLog
+
+createZones > zoneLog
+
+createPatch > patchLog
+
+renumberMesh -constant > renumberLog
+
+rm -r -f processor*
